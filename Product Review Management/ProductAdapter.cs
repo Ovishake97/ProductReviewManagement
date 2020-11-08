eb99e928117516d7bd00c8b3935b6e7e77e7b352
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-08 22:30:13 +0530

    [Abhishek] added to the UC3 branch

--- Product Review Management/ProductAdapter.cs
+++ Product Review Management/ProductAdapter.cs
@@ -20,5 +20,16 @@ namespace Product_Review_Management
                 Console.WriteLine("------------");
             }
         }
+        /// Method to retrieve the records whose rating is greater than 3
+        /// and product id is either 1 or 4 or 9
+        public static void GetRecordsForRatings(List<ProductReview> list) {
+            var data = (list.Where(r => r.rating > 3 && r.productID == 1 || r.productID == 4 || r.productID == 9));
+            foreach (var element in data) {
+                Console.WriteLine(element.productID);
+                Console.WriteLine(element.rating);
+                Console.WriteLine(element.review);
+                Console.WriteLine("-----------");
+            }
+        }
     }
 }
